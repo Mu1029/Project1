0900dc74a9b17105ba692390b51120c8b9e18029
Mu1029 <muayad.abdel@torontomu.ca>
2023-10-15 18:49:46 -0400

    Update Project1.py
    corr matrix added

--- Project1.py
+++ Project1.py
@@ -2,8 +2,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
-from mpl_toolkits.mplot3d import Axes3D
+
 
 
 # dependant variable is the step #, independant variables are the coordinates (X,Y,Z)
@@ -79,7 +80,41 @@ ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 ax.set_title('3D-coordinate Scatter Plot')
 
+#plt.figure()
 
+
+#  Step 3
+
+ # train/test 80/20 data split
+
+X_matrix = df[['X', 'Y', 'Z']]
+print(X_matrix)
+
+X_matrix = df.drop(columns=['Step'])
+print(X_matrix)
+
+y = df['Step']
+
+X_matrix_train, X_matrix_test, y_train, y_test = train_test_split(X_matrix, y, test_size=0.2, random_state=30)
+
+ # creating full train/test set matrices
+
+full_train_matrix = pd.DataFrame({'X': X_matrix_train['X'], 'Y': X_matrix_train['Y'], 'Z': X_matrix_train['Z'], 'Step': y_train})
+full_test_matrix = pd.DataFrame({'X': X_matrix_test['X'], 'Y': X_matrix_test['Y'], 'Z': X_matrix_test['Z'], 'Step': y_test})
+
+print(full_train_matrix)
+
+  # creating correlation matrix
+  
+correlation_matrix = full_train_matrix.corr(method='pearson')
+
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5)
+plt.show()
+
+correlation_matrix = full_train_matrix.corr(method='spearman')
+
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5)
+plt.show()
 
 
 
